4caf494048380dad58f55c8d6462776402492d43
Matt <mhilton@iso.com>
2018-07-25 17:09:03 +0100

    fixed error with apps installed as other users

--- Windows10SysPrepDebloater.ps1
+++ Windows10SysPrepDebloater.ps1
@@ -68,6 +68,7 @@ Function Remove-AppxPackagesForSysprep {
     )
     foreach ($App in $AppXApps) {
         Get-AppxPackage -Name $App | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Get-AppxPackage -Name $App -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
         Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $App | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
     }
 
