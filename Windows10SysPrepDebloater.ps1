57ef7e83b6a8902adcc813f9d752882357f5162b
Richard Newton <sycnewton@gmail.com>
2018-07-28 20:37:23 -0700

    Update Windows10SysPrepDebloater.ps1

--- Windows10SysPrepDebloater.ps1
+++ Windows10SysPrepDebloater.ps1
@@ -24,7 +24,6 @@ Function Remove-AppxPackagesForSysprep {
         "*Microsoft.OneConnect*"
         "*Microsoft.People*"
         "*Microsoft.Print3D*"
-        "**"
         "*Microsoft.SkypeApp*"
         "*Microsoft.StorePurchaseApp*"
         "*Microsoft.WindowsAlarms*"
