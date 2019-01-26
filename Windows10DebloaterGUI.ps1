5f726f50dc493cb227aa0d0f6139bdf923f3d1ca
Richard Newton <sycnewton@gmail.com>
2019-01-25 20:22:42 -0800

    Added code to have script self elevate to administrator
    I added code that checks if the script was run as an Administrator, and if not then it self elevates with a UAC prompt asking if it can be run as an Administrator. The script works best when run as an Adminstrator.

--- Windows10DebloaterGUI.ps1
+++ Windows10DebloaterGUI.ps1
@@ -1,3 +1,11 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "&" + $MyInvocation.MyCommand.Definition + "" 
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator." -ForegroundColor "White"
+    Start-Sleep 1
+    Start-Process "powershell.exe" -Verb RunAs -ArgumentList $arguments
+    Break
+}
+
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
