a4495be889e9da258ebf5ab5b5fa892f46449d34
Richard Newton <sycnewton@gmail.com>
2019-02-09 14:46:42 -0800

    Added "*Dolby*" to be removed by script

--- Windows10Debloater.ps1
+++ Windows10Debloater.ps1
@@ -103,6 +103,7 @@ Function DebloatBlacklist {
         "*Royal Revolt*"
         "*Sway*"
         "*Speed Test*"
+        "*Dolby*"
              
         #Optional: Typically not removed but you can if you need to for some reason
         #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
