c09b8185d897041c477aa19c9e81870e75112778
Richard Newton <sycnewton@gmail.com>
2019-02-23 14:28:00 -0800

    Update Windows10DebloaterGUI.ps1

--- Windows10DebloaterGUI.ps1
+++ Windows10DebloaterGUI.ps1
@@ -1,11 +1,11 @@
 #This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
-<#If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     $arguments = "&" + $MyInvocation.MyCommand.Definition + "" 
     Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator." -ForegroundColor "White"
     Start-Sleep 1
     Start-Process "powershell.exe" -Verb RunAs -ArgumentList $arguments
     Break
-}#>
+}
 
 <# This form was created using POSHGUI.com  a free online gui designer for PowerShell
 .NAME
