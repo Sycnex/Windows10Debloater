b444907ce4f07162f52fcf6fe25b078bab00d07a
me2345 <jake01756@gmail.com>
2019-06-17 12:05:54 -0400

    Source code for debloater,exe

---
+++ installer run.bat
@@ -0,0 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBFVRwHSAE+/Fb4I5/jH2OOJpkIKR7NvXIrZ1ruPNOUBp0jlZoIoxEZzm8QCHydRfR2lP1x6oG1N1g==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBFVRwHSAE+/Fb4I5/jH2OOJpkIKR7NvXIrZ1ruPNOUBp0jlZoIoxEZenckAAxpMewDlaxcxyQ==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo Running Script, please wait...
+cd Windows 10 Debloater
+powershell
+Set-ExecutionPolicy Unrestricted -Force
+Windows10Debloater.ps1
