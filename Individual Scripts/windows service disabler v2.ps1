a9f5e503d5a8aeaa5a81056dceb7c2ea1b05b1f3
Brandon <74426342+Brandonbr1@users.noreply.github.com>
2021-06-29 18:35:57 -0700

    requested changes
    added the requested changes you suggested

--- Individual Scripts/windows service disabler v2.ps1
+++ Individual Scripts/windows service disabler v2.ps1
@@ -74,11 +74,21 @@ $services = @(
     #"StorSvc"                                       #Disables StorSvc (usb external hard drive will not be reconised by windows)
     "RtkBtManServ"                                  #Disables Realtek Bluetooth Device Manager Service
     "QWAVE"                                         #Disables Quality Windows Audio Video Experience (audio and video might sound worse)
- 
-
-
-
-
+     #hp services
+    "HPAppHelperCap"
+    "HPDiagsCap"
+    "HPNetworkCap"
+    "HPSysInfoCap"
+    "HpTouchpointAnalyticsService"
+    #hyper-v services
+     "HvHost"                          
+    "vmickvpexchange"
+    "vmicguestinterface"
+    "vmicshutdown"
+    "vmicheartbeat"
+    "vmicvmsession"
+    "vmicrdv"
+    "vmictimesync" 
     # Services which cannot be disabled
     #"WdNisSvc"
 )
@@ -87,41 +97,6 @@ foreach ($service in $services) {
     Write-Output "Trying to disable $service"
     Get-Service -Name $service | Set-Service -StartupType Disabled
 }
-
-$hyper = @(
-    #hyper-v services
-    "HvHost"                          
-    "vmickvpexchange"
-    "vmicguestinterface"
-    "vmicshutdown"
-    "vmicheartbeat"
-    "vmicvmsession"
-    "vmicrdv"
-    "vmictimesync" 
-
-)
-
-
-foreach ($hyper in $hyper) {
-    Write-Output "Trying to disable $hyper-v"
-    Get-Service -Name $service | Set-Service -StartupType Disabled
-}
-
-$hp = @(
-     #hp services
-    "HPAppHelperCap"
-    "HPDiagsCap"
-    "HPNetworkCap"
-    "HPSysInfoCap"
-    "HpTouchpointAnalyticsService"
-)
-
- foreach ($hp in $hp) {
-    Write-Output "Trying to disable $hp"
-    Get-Service -Name $service | Set-Service -StartupType Disabled
-}
-
-
 
 #stop service this stop the services
 Get-Service diagnosticshub.standardcollector.service | Where {$_.status –eq 'Stopped'} |  Stop-Service
@@ -139,12 +114,67 @@ Get-Service WMPNetworkSvc | Where {$_.status –eq 'Stopped'} |  Stop-Service
 Get-Service wscsvc | Where {$_.status –eq 'Stopped'} |  Stop-Service
 Get-Service WSearch | Where {$_.status –eq 'Stopped'} |  Stop-Service
 Get-Service XblAuthManager | Where {$_.status –eq 'Stopped'} |  Stop-Service
-
-
-
-
-
-
-
-
-
+Get-Service XblGameSave | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service XboxNetApiSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service XboxGipSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service ndu | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service WerSvc | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service Spooler | Where {$_.status –eq 'running'} |  Stop-Service   
+Get-Service Fax | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service fhsvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service gupdate | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service gupdatem | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service stisvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service AJRouter | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service MSDTC | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service dmwappushservice | Where {$_.status –eq 'running'} |  Stop-Service   
+Get-Service WpcMonSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service PhoneSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service PrintNotify | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service PcaSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service WPDBusEnum | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service LicenseManager | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service seclogon | Where {$_.status –eq 'running'} |  Stop-Service   
+Get-Service SysMain | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service lmhosts | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service wisvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service FontCache | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service RetailDemo | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service ALG | Where {$_.status –eq 'running'} |  Stop-Service 
+#Get-Service BFE | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service BrokerInfrastructure | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service SCardSvr | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service lfsvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service EntAppSvc | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service BthAvctpSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service BDESVC | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service iphlpsvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service edgeupdate | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service MicrosoftEdgeElevationService | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service edgeupdatem | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service SEMgrSvc | Where {$_.status –eq 'running'} |  Stop-Service 
+#Get-Service PNRPsvc | Where {$_.status –eq 'running'} |  Stop-Service 
+#Get-Service p2psvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service p2pimsvc | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service PerfHost | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service BcastDVRUserService_48486de | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service CaptureService_48486de | Where {$_.status –eq 'running'} |  Stop-Service 
+Get-Service cbdhsvc_48486de | Where {$_.status –eq 'running'} |  Stop-Service  
+Get-Service BluetoothUserService_48486de | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service WpnService | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service StorSvc | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service QWAVE | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service RtkBtManServ | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HPAppHelperCap | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HPDiagsCap | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HPNetworkCap | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HPSysInfoCap | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HpTouchpointAnalyticsService | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service HvHost | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmickvpexchange | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmicguestinterface | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmicshutdown | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmicheartbeat | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmicvmsession | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmicrdv | Where {$_.status –eq 'running'} |  Stop-Service
+Get-Service vmictimesync | Where {$_.status –eq 'running'} |  Stop-Service
